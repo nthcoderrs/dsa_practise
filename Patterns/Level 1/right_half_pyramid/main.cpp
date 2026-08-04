8773de9310329c5af2cc23d924df63836c34ea93
Prince <prince9goswami@gmail.com>
2026-08-04 21:26:57 +0530

    Lvl 1 Pattern - Right Half Pyramid

---
+++ Patterns/Level 1/right_half_pyramid/main.cpp
@@ -0,0 +1,56 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n) {
+
+    for(int i = 0; i < n; i++) {
+
+        for(int j = 0; j < n - i - 1; j++) {
+            cout << "  ";
+        }
+
+        for(int j = 0; j <= i; j++) {
+            cout << "* ";
+        }
+
+        cout << endl;
+    }
+}
+
+int main() {
+
+    int testCase;
+    cin >> testCase;
+
+    while(testCase--) {
+        int n;
+        cin >> n;
+        pattern(n);
+        cout << endl << endl;
+    }
+
+    return 0;
+}
+
+/*
+Sample Input
+2
+3
+5
+
+Sample Output
+
+    *
+  * *
+* * *
+
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+
+Time Complexity : O(n²)
+Space Complexity: O(1)
+
+*/

5ed65b169f2c6e5ec088a2c37b2e9b6724d0d26f
Prince <prince9goswami@gmail.com>
2026-08-04 12:30:06 +0530

    Level 1 - Pattern 1 [ Hollow Square ]

---
+++ Patterns/Level 1/hollow_square/main.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n) {
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+}
+
+int main() {
+
+    int testCase;
+    cin >> testCase;
+
+    for (int i = 0; i < testCase; i++) {
+        int n;
+        cin >> n;
+        pattern(n);
+        cout << endl << endl;
+    }
+
+    return 0;
+}
+
+/*
+Sample Input
+2
+3
+5
+
+Sample Output
+
+* * *
+* * *
+* * *
+
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+
+Time Complexity : O(n²)
+Space Complexity: O(1)
+
+Where,
+n = Number of rows = Number of columns
+*/

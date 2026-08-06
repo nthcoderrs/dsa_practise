5612b95b72a4649d78af54fc9fe9e75d66b179ae
Prince <prince9goswami@gmail.com>
2026-08-06 22:50:14 +0530

    Lvl 4 Pattern - Diamond

---
+++ Patterns/Level 4/diamond/main.cpp
@@ -0,0 +1,66 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n) {
+
+    for(int i = 0; i < 2 * n - 1; i++) {
+
+        int row;
+
+        if(i < n)
+            row = i;
+        else
+            row = 2 * n - i - 2;
+
+        for(int j = 0; j < n - row - 1; j++)
+            cout << " ";
+
+        for(int j = 0; j < 2 * row + 1; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+}
+
+int main() {
+
+    int testCase;
+    cin >> testCase;
+
+    while(testCase--) {
+        int n;
+        cin >> n;
+        pattern(n);
+        cout << endl << endl;
+    }
+
+    return 0;
+}
+
+/*
+Sample Input
+2
+3
+5
+
+Sample Output
+
+  *
+ ***
+*****
+ ***
+  *
+
+    *
+   ***
+  *****
+ *******
+*********
+ *******
+  *****
+   ***
+    *
+
+Time Complexity : O(n²)
+Space Complexity: O(1)
+*/

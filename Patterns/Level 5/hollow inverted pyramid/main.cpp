382bea09ab9921bdb51607d2032a3443ff951fdd
Prince <prince9goswami@gmail.com>
2026-08-06 23:39:46 +0530

    Lvl 5 Pattern - Hollow Inverted Pyramid

---
+++ Patterns/Level 5/hollow inverted pyramid/main.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n) {
+
+    for(int i = 0; i < n; i++) {
+
+        // Spaces
+        for(int j = 0; j < i; j++)
+            cout << " ";
+
+        // Stars
+        for(int j = 0; j < 2 * (n - i) - 1; j++) {
+
+            if(i == 0 || j == 0 || j == 2 * (n - i) - 2)
+                cout << "*";
+            else
+                cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+int main() {
+
+    int testCase;
+    cin >> testCase;
+
+    while(testCase--) {
+        int n;
+        cin >> n;
+        pattern(n);
+        cout << endl << endl;
+    }
+
+    return 0;
+}
+
+/*
+Sample Input
+2
+3
+5
+
+Sample Output
+
+*****
+ * *
+  *
+
+*********
+ *     *
+  *   *
+   * *
+    *
+
+Time Complexity : O(n²)
+Space Complexity: O(1)
+*/

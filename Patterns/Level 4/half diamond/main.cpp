0332561862dd21ca1c273d1943e986be42c90f55
Prince <prince9goswami@gmail.com>
2026-08-06 22:56:34 +0530

    Lvl 4 pattern - half diamond

---
+++ Patterns/Level 4/half diamond/main.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n) {
+
+    for(int i = 0; i < 2 * n - 1; i++) {
+
+        int row;
+
+        if(i < n)
+            row = i;
+        else
+            row = 2 * n - i - 2;
+
+        for(int j = 0; j <= row; j++)
+            cout << "*";
+
+        cout << endl;
+    }
+}
+
+int main() {
+
+    int testCase;
+    cin >> testCase;
+
+    while(testCase--) {
+        int n;
+        cin >> n;
+        pattern(n);
+        cout << endl << endl;
+    }
+
+    return 0;
+}
+
+/*
+Sample Input
+2
+3
+5
+
+Sample Output
+
+*
+**
+***
+**
+*
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+
+Time Complexity : O(n²)
+Space Complexity: O(1)
+
+*/

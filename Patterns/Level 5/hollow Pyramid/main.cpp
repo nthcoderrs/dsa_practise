929958fb29d113036e301fb126c2ccbd81f73b11
Prince <prince9goswami@gmail.com>
2026-08-07 21:40:25 +0530

    Lvl 5 Pattern - hollow Pyramid

---
+++ Patterns/Level 5/hollow Pyramid/main.cpp
@@ -0,0 +1,61 @@
+#include<iostream>
+using namespace std;
+
+void pattern(int n) {
+
+    for(int i = 0; i < n; i++) {
+
+        // Spaces
+        for(int j = 0; j < n - i - 1; j++)
+            cout << " ";
+
+        // Stars
+        for(int j = 0; j < 2 * i + 1; j++) {
+
+            if(i == n - 1 || j == 0 || j == 2 * i)
+                cout << "*";
+            else
+                cout << " ";
+        }
+
+        cout << endl;
+    }
+}
+
+int main() {
+
+    int testCase;
+    cin >> testCase;
+
+    while(testCase--) {
+        int n;
+        cin >> n;
+        pattern(n);
+        cout << endl << endl;
+    }
+
+    return 0;
+}
+
+/*
+Sample Input
+2
+3
+5
+
+Sample Output
+
+  *
+ * *
+*****
+
+    *
+   * *
+  *   *
+ *     *
+*********
+
+Time Complexity : O(n²)
+Space Complexity: O(1)
+
+*/
